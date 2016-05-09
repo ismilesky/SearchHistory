4d7fd22e514c1ff5649812f3eb05d41c6e3daf8f
ismilesky <fighter_kongfei@126.com>
2016-05-09 10:10:50 +0800

    Initial Commit

---
+++ SearchHistory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SearchHistory
+//
+//  Created by VS on 16/5/9.
+//  Copyright © 2016年 VS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SearchHistory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SearchHistory
+//
+//  Created by VS on 16/5/9.
+//  Copyright © 2016年 VS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
